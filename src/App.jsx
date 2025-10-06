3f619a24c3a1119fd9b0cbc0b09cb1fe3e1b4948
Felix <felixmarcellino51@gmail.com>
2025-10-06 12:47:57 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { Navbar } from './components/navbar.jsx'
+import HomePageLight from './assets/Home_Page.png'
+import HomePageDark from './assets/Home_Page_Dark.png'
+import HeroImage from './assets/Hero_Vectra.png'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <section className='hero-section'>
+        <div className='hero-content'>
+          <img src={HeroImage} alt="" className='hero-image'/>
+          <div className='hero-text-container'>
+          <h1 className='hero-text'>The future of world is here</h1>
+          <p className='hero-subtext'>We're the most trusted place for people and businesses to buy, sell, and manage crypto.</p>
+          </div>
+        </div>
+      </section>
+      <main>
+
+      </main>
+    </>
+  )
+}
+
+export default App
