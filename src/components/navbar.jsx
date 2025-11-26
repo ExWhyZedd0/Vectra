eff305c9da00fc96b3aad9b2231958bb05f382f6
Felix <felixmarcellino51@gmail.com>
2025-11-27 01:15:58 +0700

    Update navbar layout and menu items
    Refactored the navbar to group the logo and menu in a flex container for improved alignment. Updated navigation links to 'Market', 'News', 'FAQ', and 'About Us'. Also fixed the page title in index.html to 'Vectra - Future World'.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,26 +8,41 @@ export const Navbar = () => {
 
   return (
     <nav>
+      <div className="logo-menu">
       <Link to="/" className="title">
         <img src={Logo} alt="Vectra Logo" className="logo" />
         <p className="logo-text">Vec<span>tra</span></p>
       </Link>
-      <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
-        <span></span>
-        <span></span>
-        <span></span>
+        <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <ul className={menuOpen ? "open" : ""}>
+          <li>
+            <NavLink to="/market">Market</NavLink>
+          </li>
+          <li>
+            <NavLink to="/news">News</NavLink>
+          </li>
+          <li>
+            <NavLink to="/faq">FAQ</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about-us">About Us</NavLink>
+          </li>
+        </ul>
       </div>
-      <ul className={menuOpen ? "open" : ""}>
-        <li>
-          <NavLink to="/about">About</NavLink>
-        </li>
-        <li>
-          <NavLink to="/services">Services</NavLink>
-        </li>
-        <li>
-          <NavLink to="/contact">Contact</NavLink>
-        </li>
-      </ul>
+      {/* <div className="login-signup">
+        <ul className={menuOpen ? "open" : ""}>
+          <li>
+            <Navlink to="/login">Login</Navlink>
+          </li>
+          <li>
+            <Navlink to="/signup">Sign Up</Navlink>
+          </li>
+        </ul>
+      </div> */}
     </nav>
   );
 };
